7c455b41906a3354761f19c0afcf1ca44399d710
EzequielJimenez <ezequielemilianojimenez@gmail.com>
2026-07-04 18:08:04 -0300

    Initial Site Creation

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-
-// Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,22 +10,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/**
-  * Configure Express middleware
-  */
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
-
-// Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
 
-
-/**
-  * Routes
-  */
 app.get('/', async (req, res) => {
     const title = 'Home';
     res.render('home', { title });
@@ -42,6 +27,11 @@ app.get('/organizations', async (req, res) => {
 app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
+});
+
+app.get('/categories', async (req, res) => {
+    const title = 'Service Categories';
+    res.render('categories', { title });
 });
 
 app.listen(PORT, () => {
